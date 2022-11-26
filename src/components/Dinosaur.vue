ae296056936ed79bb16bd859a0c4b69b8a95c0b8
electric-mike <66960109+electric-mike@users.noreply.github.com>
2022-11-26 14:48:24 -0500

    Fetch dino on direct page load

--- src/components/Dinosaur.vue
+++ src/components/Dinosaur.vue
@@ -4,7 +4,19 @@ import { store } from '../store.js';
 export default {
   data() {
     return {
-      store
+      store,
+      loading: false
+    }
+  },
+
+  async mounted() {
+    const dinoName = this.$router.currentRoute._value.params.dinosaur
+    if (!store?.dinosaur?.name) {
+      this.loading = true
+      const res = await fetch(`http://localhost:8000/api/${dinoName}`)
+      const dinosaur = await res.json();
+      this.store.dinosaur = dinosaur
+      this.loading = false
     }
   }
 }
@@ -12,7 +24,12 @@ export default {
 
 
 <template>
-  Name: {{ store.dinosaur.name }}
-  <br />
-  Description: {{ store.dinosaur.description }}
+  <div v-if="store.dinosaur.name">
+    Name: {{ store.dinosaur.name }}
+    <br />
+    Description: {{ store.dinosaur.description }}
+  </div>
+  <div v-else-if="loading">
+    Loading...
+  </div>
 </template>
